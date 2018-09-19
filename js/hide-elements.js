bb4136af3d015f49f671f8dd427cda771d2802f3
myvlink <themyvlink@gmail.com>
2018-09-19 21:40:48 +0500

    обновил

--- js/hide-elements.js
+++ js/hide-elements.js
@@ -1,15 +1,22 @@
 'use strict';
 (function () {
-
+  var screenHeigth = document.documentElement.clientHeight;
   // Скрытие кнопки вверх
-  var aboutMeBlock = document.querySelector('.about-me');
+  var pageHeaderBlock = document.querySelector('.page-header');
   var pageFooterLinkToUp = document.querySelector('.page-footer__link-to-up');
+  var pageFooterBlock = document.querySelector('.page-footer');
   pageFooterLinkToUp.classList.add('page-footer__link-to-up--hidden');
   window.onscroll = window.utils.debounce(function () {
-    if (aboutMeBlock.getBoundingClientRect().top < 0) {
+    if (pageHeaderBlock.getBoundingClientRect().top < 0) {
       pageFooterLinkToUp.classList.remove('page-footer__link-to-up--hidden');
     } else {
       pageFooterLinkToUp.classList.add('page-footer__link-to-up--hidden');
+    }
+
+    if (pageFooterBlock.getBoundingClientRect().top < screenHeigth) {
+      pageFooterLinkToUp.classList.add('page-footer__link-to-up--stopped');
+    } else {
+      pageFooterLinkToUp.classList.remove('page-footer__link-to-up--stopped');
     }
   });
 })()
